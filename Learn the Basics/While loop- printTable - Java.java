f1213672b021ff0eb801bf079bd9e80e874e62d9
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-11-10 18:31:40 +0530

    While loop- printTable - Java

---
+++ Learn the Basics/While loop- printTable - Java.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 11;
+        while((c--)>1){
+            System.out.print(c*n + " ");
+        }
+    }
+}
