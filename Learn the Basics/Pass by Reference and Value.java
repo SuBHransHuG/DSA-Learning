99c8b8fbaad4f932064d0613e9c04641a0436a9d
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-11-10 18:42:10 +0530

    Pass by Reference and Value

---
+++ Learn the Basics/Pass by Reference and Value.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+     
+        int[] n = new int[2];
+        n[0] = a+1;
+        n[1] = b+2;
+        return n;
+    }
+}
